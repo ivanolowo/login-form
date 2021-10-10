ee2d6e1516eaf55703bd435ba33022eb18ec960e
Ivan Olwo <ivolowo@gmail.com>
2021-10-10 17:19:30 +0200

    added error style

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -11,15 +11,13 @@ function LoginForm({ Login, error }) {
             <form onSubmit={submitHandler}>
                 <div className="form-inner">
                 <h2>Aline Learn better!</h2>
-                {(error != "") ? ( <div className="error">{error}</div>) : ""}
+                {(error != "") ? (<div className="error">{error}</div>) : ""}
+                
                     <div className="form-group">
                         <label htmlFor="username">Username:</label>
                         <input type="text" name="username" id="username" onChange={e => setDetails({...details, username: e.target.value})} value={details.username} />
                     </div>
-                   {/*  <div className="form-group">
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email} />
-                    </div> */}
+ 
                     <div className="form-group">
                         <label htmlFor="password">Password:</label>
                         <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password} />
